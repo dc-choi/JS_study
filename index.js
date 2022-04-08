15ad3fe908fc939261bf2df3df5efe6398e230aa
dcchoi <cheek0805@naver.com>
2022-04-08 19:20:36 +0900

    인스턴스 예제 실습

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * @function getName
  * 1. 
  * @param { String } code
+ * @return { String } name
  */
 
 let idx = 0; // for 루프를 돌리기 위한 변수
@@ -20,3 +21,12 @@ for (idx; idx < 50; idx++) {
 console.log(`홀수의 누적 값: ${odd}`);
 console.log(`짝수의 누적 값: ${even}`);
 console.log(`총 누적 값: ${sum}`);
+
+let number = 10;
+let objNumber = Number(10);
+
+console.log(typeof number);
+console.log(typeof objNumber); // new 연산자를 붙이면 인스턴스로 생성됨.
+console.log(number === objNumber);
+console.log(typeof 20..toString()); // IEEE 754 표준으로 인해 JS는 숫자를 실수형으로 표시함.
+console.log(typeof number.toString());
