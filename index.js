df69c56125223d6a06cc72af7dd69c1967267139
dc-choi <cheek0805@naver.com>
2022-09-26 14:57:47 +0900

    엔진 처리 확인

--- index.js
+++ index.js
@@ -63,3 +63,54 @@ console.log(end - start);
 
 let data = { data: 'qwer', data2: 'qwerqwerqwer'};
 console.log(JSON.stringify(data, null, 4));
+
+/**
+ * 브라우저에서 실행해야하는 코드
+ */
+function book() {
+	// debugger;
+	// var title = 'JS';
+	// function getBook() {
+	// 	return title;
+	// }
+	// var readBook = function(){};
+	// getBook();
+}
+
+book();
+
+// 선언문은 두번 선언하면 선언마다 갱신된다. 그래서 책2가 나옴.
+function getBook() {
+	return '책1';
+}
+console.log(getBook());
+function getBook() {
+	return '책2';
+}
+
+// 표현식은 두번 선언할 경우 이미 값이 있다고 판단하여 맨 처음만 할당된다. 책1이 나옴.
+var getBook2 = function() {
+	return '책1';
+}
+console.log(getBook2());
+var getBook2 = function() {
+	return '책2';
+};
+
+// 선언문이 JS 엔진상 먼저 읽히기 때문에 책2가 나온다.
+function getBook3() {
+	return '책2';
+}
+console.log(getBook3());
+var getBook3 = function() {
+	return '책1';
+}
+
+// 선언문을 먼저 읽지만 표현식을 초기화하면서 선언문으로 할당한 것이 초기화됨.
+var getBook4 = function() {
+	return '책1';
+}
+console.log(getBook4());
+function getBook4() {
+	return '책2';
+}
