ffa6908eaba43ba321372552df94d42b6a026977
dc-choi <cheek0805@naver.com>
2022-11-03 09:59:16 +0900

    드림 코딩 Class 강의를 듣고 실습
    각각 getter, setter, 메서드, 프로퍼티로서 사용하는 방법에 대해 숙지하고
    어떤 방식이 더 효율적인지 공부했습니다.

---
+++ class_test.js
@@ -0,0 +1,46 @@
+﻿/**
+ * User
+ * 
+ * @param {string} lastname
+ * @param {string} firstname
+ * @param {number} age
+ * 
+ * @return {User} User
+ */
+class User {
+	constructor(lastname = '', firstname = '', age = 1) {
+		this.lastname = lastname;
+		this.firstname = firstname;
+		this.age = age;
+	}
+
+	get age() {
+		return this._age;
+	}
+
+	set age(value) {
+		this._age = value <= 1 ? 1 : value
+	}
+
+	get fullname() {
+		return `${this.firstname} ${this.lastname}`;
+	}
+
+	getfullname() {
+		return `${this.firstname} ${this.lastname}`;
+	}
+
+	getFullName = () => {
+		return `${this.firstname} ${this.lastname}`;
+	}
+}
+
+const dongchul = new User('choi', 'dongchul', 0);
+
+console.log(dongchul); // 인스턴스
+console.log(dongchul.firstname); // 프로퍼티 호출
+console.log(dongchul.fullname); // getter 호출
+console.log(dongchul.getfullname()); // method 호출
+console.log(dongchul.getFullName()); // 프로퍼티 호출
+console.log(dongchul.age); // getter 호출
+console.log(dongchul._age); // 프로퍼티 호출
