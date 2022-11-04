8f79e8376d1241186315a361f37666041e12ca4e
dc-choi <cheek0805@naver.com>
2022-11-04 13:31:03 +0900

    인증시 검증부분 로직 작성
    회사에서 인증 부분을 파이프라인식으로 처리하는 것을 확인하였고,
    해당 부분은 토큰을 파싱 후, 데이터를 검증하는 로직중 하나입니다.

---
+++ date_test.js
@@ -0,0 +1,34 @@
+﻿const MILLISECONDS_PER_SECOND = 1000;
+const SECONDS_PER_MINUTE = 60;
+const MINUTES_PER_HOUR = 60;
+
+const SECOND = MILLISECONDS_PER_SECOND;
+const MINUTE = SECOND * SECONDS_PER_MINUTE;
+const HOUR = MINUTE * MINUTES_PER_HOUR;
+
+const regex = /[^A-z]/g;
+const expire = '12h';
+const now = new Date().getTime();
+const expireDate = new Date('2022-11-02T06:04:43.951Z').getTime();
+const str = expire.replace(regex, "");
+let result;
+
+if (str === 'h') {
+	let parsed = expire.split('h');
+	result = (now - expireDate) > (parseInt(parsed[0]) * HOUR);
+} else if (str === 'm') {
+	let parsed = expire.split('m');
+	result = (now - expireDate) > (parseInt(parsed[0]) * MINUTE);
+} else {
+	console.log('error');
+}
+
+console.log(MINUTE);
+console.log(HOUR);
+console.log(result);
+
+if (result) {
+	console.log('error');
+} else {
+	console.log('not error');
+}
