4e1eb641680089d9960b025e5f086143a5d62f73
dc-choi <cheek0805@naver.com>
2022-11-07 21:37:17 +0900

    클래스와 프로토타입 차이점 발표 준비
    발표 준비를 하면서 작성한 예제 코드를 커밋

---
+++ class.js
@@ -0,0 +1,36 @@
+﻿
+class Person{
+	constructor(name) {
+		this.name = name;
+	}
+
+	sayHi() {
+		console.log(`Hi~~ I'm ${this.name}`);
+	}
+}
+
+class Developer extends Person {
+	constructor(name) {
+		super(name);
+	}
+
+	coding() {
+		console.log(`coding... I'm ${this.name}`);
+	}
+}
+
+class BackEndDeveloper extends Developer {
+	constructor(name) {
+		super(name);
+	}
+
+	nodeJS() {
+		console.log(`nodeJS... I'm ${this.name}`);
+	}
+}
+
+const dongchul = new BackEndDeveloper('dongchul');
+dongchul.sayHi();
+dongchul.coding();
+dongchul.nodeJS();
+
---
+++ prototype.js
@@ -0,0 +1,29 @@
+﻿
+function Person(name) {
+	this.name = name
+}
+Person.prototype.sayHi = function() {
+	console.log(`Hi~~ I'm ${this.name}`);
+}
+
+function Developer(name) {
+	Person.call(this, name);
+}
+Developer.prototype = Object.create(Person.prototype, {});
+Developer.prototype.coding = function() {
+	console.log(`coding... I'm ${this.name}`);
+}
+
+function BackEndDeveloper(name) {
+	Developer.call(this, name);
+}
+BackEndDeveloper.prototype = Object.create(Developer.prototype, {});
+BackEndDeveloper.prototype.nodeJS = function() {
+	console.log(`nodeJS... I'm ${this.name}`);
+}
+
+const dongchul = new BackEndDeveloper('dongchul');
+dongchul.sayHi();
+dongchul.coding();
+dongchul.nodeJS();
+
