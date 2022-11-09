e867b0cf9c56dc39c765998103fbb4d73002df55
dc-choi <cheek0805@naver.com>
2022-11-09 14:05:17 +0900

    동작하지않는 코드의 예시
    클래스 프로토타입의 발표를 하면서 작성했던 동작하지 않는 코드의 예시

---
+++ example.js
@@ -0,0 +1,11 @@
+﻿
+function Person(name) {
+	this.name = name
+}
+Person.sayHi = function() {
+	console.log(`Hi~~ I'm ${this.name}`);
+}
+
+const dongchul = new Person('dongchul');
+dongchul.sayHi();
+
