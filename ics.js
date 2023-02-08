ad664a0b97d3d7c4a29e274d68963a0830097be8
dc-choi <cheek0805@naver.com>
2023-02-08 12:22:30 +0900

    숫자로 형변환시 특이점 확인

--- ics.js
+++ ics.js
@@ -1,3 +1,11 @@
 // YYYYMMDD 형식으로 출력
 const splitDate = new Date().toISOString().split('T');
 console.log(splitDate[0].replace(/\-/g, ''));
+
+const parseStr = Number(undefined);
+const parseStr2 = Number(null);
+const parseStr3 = Number('');
+
+console.log(parseStr);
+console.log(parseStr2);
+console.log(parseStr3);
