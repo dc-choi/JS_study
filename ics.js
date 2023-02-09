566c2d9be82adde13577ce1146255cdc27a20e0a
dc-choi <cheek0805@naver.com>
2023-02-09 17:06:04 +0900

    !! 연산자 확인하기
    !! 연산자를 통해서 더 간편하게 예외처리를 할 수 있도록 공부하였습니다.

--- ics.js
+++ ics.js
@@ -1,5 +1,7 @@
 // YYYYMMDD 형식으로 출력
+const date = new Date().toISOString();
 const splitDate = new Date().toISOString().split('T');
+console.log(date);
 console.log(splitDate[0].replace(/\-/g, ''));
 
 const parseStr = Number(undefined);
@@ -9,3 +11,13 @@ const parseStr3 = Number('');
 console.log(parseStr);
 console.log(parseStr2);
 console.log(parseStr3);
+
+const checkBool = !!!undefined;
+const checkBool2 = !!!null;
+const checkBool3 = !!!'';
+const checkBool4 = !!!0;
+
+console.log(checkBool);
+console.log(checkBool2);
+console.log(checkBool3);
+console.log(checkBool4);
